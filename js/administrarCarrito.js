44206cef8603615a967de3a965aa28e7cb638486
Fabián Edgardo Salomone <fsalomone@cgp.ar>
2022-08-09 09:15:34 -0300

    Creacion de proyecto

---
+++ js/administrarCarrito.js
@@ -0,0 +1,155 @@
+//Declaración de variables globales
+// let productos = [];
+// let productosCarrito = [];
+// let detalleProductos = [];
+// let cuponDescuento = "";
+// let descripcionDescuentoProducto = "";
+// let descripcionProductos = "";
+// let descripcionPromptAgregarProductos = "";
+// let descripcionPromptProducto = "";
+// let importeTotalSinDescuentos = 0;
+// let opcion;
+// let codigoProducto;
+// let cantidad;
+
+
+// class Producto{
+//     constructor(codigo, descripcion, precio, cantidad){
+//         this.codigo = codigo,
+//         this.descripcion = descripcion.toUpperCase(),
+//         this.precio = parseFloat(precio),
+//         this.cantidad = cantidad
+//     }
+
+//     implementarDescuentoDiezPorCiento(){
+//         this.precio = this.precio * 0.9;
+//     }
+// }
+
+
+// //-------- Funciones --------
+// function cargarProductosEnStock(){
+//     const producto1 = new Producto(1, "Piano", 190000, 2);
+//     const producto2 = new Producto(2, "Saxofon", 600000, 1);
+//     const producto3 = new Producto(3, "Flauta", 100000, 3);
+//     const producto4 = new Producto(4, "Guitarra", 200000, 2);
+//     const producto5 = new Producto(5, "Bateria", 700000, 2);
+
+//     productos.push(producto1, producto2, producto3, producto4, producto5);
+
+//     detalleProductos = productos.map((producto) => {
+//         return {
+//             codigo: producto.codigo,
+//             descripcion: producto.descripcion
+//         }
+//     });
+
+//     descripcionProductos = "";
+//     detalleProductos.forEach((producto) => descripcionProductos += producto.codigo + ". " + producto.descripcion + "\n");
+//     descripcionProductos += "0. Volver";
+// }
+
+// function solicitarIngreso(cadena){
+//     return prompt(cadena);
+// }
+
+// function obtenerDescripcionPromptOperacion(){
+//     return "CARRITO\n1. Agregar productos\n2. Confirmar carrito\n\nOPERACIÓN";
+// }
+
+// function obtenerDescripcionPromptAgregarProductos(detalleProductos){
+//     return "AGREGAR PRODUCTOS\n" + detalleProductos + "\n\nPRODUCTO";
+// }
+
+// function obtenerDescripcionPromptProducto(producto){
+//     return "DETALLE DE PRODUCTO SELECCIONADO" + "\nCódigo: " + producto.codigo + "\nDescripción: " + producto.descripcion + "\nPrecio: ARS " + producto.precio + "\nStock: " + producto.cantidad + "\n\nCANTIDAD"; 
+// }
+
+// function obtenerDescripcionPromptCupon(){
+//     return "INGRESE OFF2022 PARA OBTENER 10% DE DESCUENTO\n\nCUPÓN";
+// }
+
+// function informarEnConsola(mensaje){
+//     console.log(mensaje);
+// }
+
+// function aplicarDescuento(descripcionDescuento, producto){
+//     let resultado = "";
+//     if(descripcionDescuento == "OFF2022"){
+//         resultado = "ARS " + (producto.precio*0.1); 
+//         producto.implementarDescuentoDiezPorCiento();
+//         return resultado;
+//     }
+    
+//     return "No aplica";
+// }
+
+// const sumarImporte = (variable, importe) => { return variable += importe }
+
+// function calcularImporteTotalSinDescuentos(){
+//     productosCarrito.forEach((producto) => {
+//         let productoEncontrado = productos.find((prod) => prod.codigo == producto.codigo);
+//         importeTotalSinDescuentos += productoEncontrado.precio*producto.cantidad;
+//     });
+
+//     return importeTotalSinDescuentos;
+// }
+
+// function procesarAgregarProductos(){
+//     descripcionPromptAgregarProductos = obtenerDescripcionPromptAgregarProductos(descripcionProductos);
+//     codigoProducto = parseInt(solicitarIngreso(descripcionPromptAgregarProductos));
+
+//     while(codigoProducto != 0){
+//         let productoEncontrado = productos.find((producto) => producto.codigo == codigoProducto);
+        
+//         if(productoEncontrado == undefined){
+//             alert("El código de producto ingresado es incorrecto.");
+//         }
+//         else{
+//             descripcionPromptProducto = obtenerDescripcionPromptProducto(productoEncontrado);
+//             cantidad = parseInt(solicitarIngreso(descripcionPromptProducto));
+//             if(cantidad <= productoEncontrado.cantidad){
+//                 if(cantidad != 0)
+//                 {
+//                     productoEncontrado.cantidad -= cantidad;
+//                     const producto = new Producto(productoEncontrado.codigo, productoEncontrado.descripcion, productoEncontrado.precio*cantidad, cantidad);
+//                     cuponDescuento = solicitarIngreso(obtenerDescripcionPromptCupon());
+//                     descripcionDescuentoProducto = aplicarDescuento(cuponDescuento, producto);
+//                     informarEnConsola("Código: " + producto.codigo + "\nDescripción: " + producto.descripcion + "\nPrecio: ARS " + producto.precio + "\nDescuento: " + descripcionDescuentoProducto + "\nCantidad: " + producto.cantidad);
+//                     productosCarrito.push(producto);
+//                 }
+//             }
+//             else{
+//                 alert("No hay stock para la cantidad solicitada.");
+//             }
+//         }
+    
+//         codigoProducto = parseInt(solicitarIngreso(descripcionPromptAgregarProductos));
+//     }
+// }
+
+
+
+// //Comienzo
+// cargarProductosEnStock();
+
+// descripcionPrompt = obtenerDescripcionPromptOperacion();
+// opcion = solicitarIngreso(descripcionPrompt);
+// while(opcion != "2"){    
+//     switch(opcion){
+//         case "1":
+//             procesarAgregarProductos();
+//             break;
+
+//         default:
+//             alert("La opción ingresada es incorrecta.");
+//             break;
+//     }
+
+//     descripcionPrompt = obtenerDescripcionPromptOperacion();
+//     opcion = solicitarIngreso(descripcionPrompt);
+// }
+
+// importeTotalSinDescuentos = calcularImporteTotalSinDescuentos();
+// const importeTotal = productosCarrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+// informarEnConsola("IMPORTE TOTAL SIN DESCUENTOS: ARS " + importeTotalSinDescuentos + "\nIMPORTE DESCUENTOS: ARS " + (importeTotalSinDescuentos - importeTotal) + "\nIMPORTE FINAL: ARS " + importeTotal);
---
+++ js/cargarSeccionProductos.js
@@ -0,0 +1,66 @@
+class Producto{
+    constructor(imagen, marca, modelo, descripcion, cuotas, envio, precio, stock){
+        this.Imagen = imagen,
+        this.Marca = marca.toUpperCase(),
+        this.Modelo = modelo.toUpperCase(),
+        this.Descripcion = descripcion,
+        this.Cuotas = cuotas,
+        this.Envio = envio,
+        this.Precio = precio,
+        this.Stock = stock
+    }
+}
+
+
+
+const productos = [];
+
+const producto1 = new Producto("kurzweil_sp2x", "Kurzweil", "Sp2x", "Piano eléctrico Kurzweil Sp2x, 88 teclas con peso piano martillo", 6, 1500, 200000, 10);
+const producto2 = new Producto("cort_x250_bk", "Cort", "X250-bk", "Guitarra eléctrica Cort X250-BK, cuerpo sólido con dos cutaways", 12, 700, 300000, 5);
+const producto3 = new Producto("knight_jbts_100", "Knight", "Jbts-100", "Saxo tenor Knight JBTS-100 Laqueado, llave de F#", 9, 2500, 400000, 2);
+const producto4 = new Producto("focusrite_scarlett_2i4", "Focusrite", "Scarlett 2i4", "Placa de sonido Focusrite Scarlett 2i4 USB", 12, 350, 100000, 25);
+
+productos.push(producto1, producto2, producto3, producto4);
+
+let seccionProductos = document.getElementById("seccionProductos");
+
+productos.forEach((producto) => {
+    let card = document.createElement("div");
+    let imageContainer = document.createElement("div");
+    let cardImage = document.createElement("img");
+    let cardBody = document.createElement("div");
+
+    //Seteo de clase de la tarjeta
+    card.className = "col-12 col-md-6 col-lg-3 card cardProducto";
+
+    //Seteo de clase para el contenedor de la imagen
+    imageContainer.className = "cardProducto__imageContainer";
+
+    //Seteo de atributos y clase de la imagen
+    cardImage.setAttribute("src", `../assets/${producto.Imagen}.jpg`);
+    cardImage.setAttribute("alt", "Imagen de producto");
+    cardImage.className = "card-img-top cardProducto__imagen";
+
+    //Seteo de innerHTML para el cuerpo
+    let valorCuota = Math.round(producto.Precio/producto.Cuotas);
+    cardBody.innerHTML = `
+        <h5 class="card-title cardProducto__marca">${producto.Marca}</h5>
+        <h6 class="card-subtitle mb-2 text-muted cardProducto__modelo">Modelo ${producto.Modelo}</h6>
+        <p class="card-text cardProducto__descripcion">${producto.Descripcion}</p>
+        <h3 class="cardProducto__precio"><strong class="cardProducto__strongPrecio">ARS ${producto.Precio}</strong><h3/>
+        <p class="card-text cardProducto__info"><i class="fa-solid fa-credit-card cardProducto__icon"></i><small class="text-muted">${producto.Cuotas} cuotas sin interés de <strong class="cardProducto__strong">ARS ${valorCuota}</strong></small></p>
+        <p class="card-text cardProducto__info"><i class="fa-solid fa-truck cardProducto__icon"></i><small class="text-muted">Costo de envío: <strong class="cardProducto__strong">ARS ${producto.Envio}</strong></small></p>
+        <p class="card-text cardProducto__info"><i class="fa-solid fa-business-time cardProducto__icon"></i><small class="text-muted">Cantidad en stock: <strong class="cardProducto__strong">${producto.Stock}</strong></small></p>
+    `;
+    cardBody.className = "card-body cardProducto__body";
+
+    //Seteo de botón
+
+
+    //Agregaciones
+    imageContainer.appendChild(cardImage);
+    card.appendChild(imageContainer);
+    card.appendChild(cardBody);
+    card.innerHTML += `<div class="cardProducto__botonContainer"><button class="btn btn-primary cardProducto__boton" id="${producto.Modelo}">Agregar</button></div>`;
+    seccionProductos.appendChild(card);
+});
