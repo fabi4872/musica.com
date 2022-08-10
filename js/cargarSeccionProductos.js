29be92d590c8aa2cefd082f533c8aa822c99382f
Fabian Salomone <fabi4872@gmail.com>
2022-08-09 23:06:30 -0300

    Entrega

--- js/cargarSeccionProductos.js
+++ js/cargarSeccionProductos.js
@@ -1,4 +1,4 @@
-class Producto{
+class ProductoStock{
     constructor(imagen, marca, modelo, descripcion, cuotas, envio, precio, stock){
         this.Imagen = imagen,
         this.Marca = marca.toUpperCase(),
@@ -13,18 +13,18 @@ class Producto{
 
 
 
-const productos = [];
+const productosStock = [];
 
-const producto1 = new Producto("assets/kurzweil_sp2x.jpg", "Kurzweil", "Sp2x", "Piano eléctrico Kurzweil Sp2x, 88 teclas con peso piano martillo", 6, 1500, 200000, 10);
-const producto2 = new Producto("assets/cort_x250_bk.jpg", "Cort", "X250-bk", "Guitarra eléctrica Cort X250-BK, cuerpo sólido con dos cutaways", 12, 700, 300000, 5);
-const producto3 = new Producto("assets/knight_jbts_100.jpg", "Knight", "Jbts-100", "Saxo tenor Knight JBTS-100 Laqueado, llave de F#", 9, 2500, 400000, 2);
-const producto4 = new Producto("assets/focusrite_scarlett_2i4.jpg", "Focusrite", "Scarlett 2i4", "Placa de sonido Focusrite Scarlett 2i4 USB", 12, 350, 100000, 25);
+const producto1 = new ProductoStock("assets/kurzweil_sp2x.jpg", "Kurzweil", "Sp2x", "Piano eléctrico Kurzweil Sp2x, 88 teclas con peso piano martillo", 6, 1500, 200000, 10);
+const producto2 = new ProductoStock("assets/cort_x250_bk.jpg", "Cort", "X250-bk", "Guitarra eléctrica Cort X250-BK, cuerpo sólido con dos cutaways", 12, 700, 300000, 5);
+const producto3 = new ProductoStock("assets/knight_jbts_100.jpg", "Knight", "Jbts-100", "Saxo tenor Knight JBTS-100 Laqueado, llave de F#", 9, 2500, 400000, 2);
+const producto4 = new ProductoStock("assets/focusrite_scarlett_2i4.jpg", "Focusrite", "Scarlett 2i4", "Placa de sonido Focusrite Scarlett 2i4 USB", 12, 350, 100000, 25);
 
-productos.push(producto1, producto2, producto3, producto4);
+productosStock.push(producto1, producto2, producto3, producto4);
 
 let seccionProductos = document.getElementById("seccionProductos");
 
-productos.forEach((producto) => {
+productosStock.forEach((producto) => {
     let card = document.createElement("div");
     let imageContainer = document.createElement("div");
     let cardImage = document.createElement("img");
@@ -53,9 +53,6 @@ productos.forEach((producto) => {
         <p class="card-text cardProducto__info"><i class="fa-solid fa-business-time cardProducto__icon"></i><small class="text-muted">Cantidad en stock: <strong class="cardProducto__strong">${producto.Stock}</strong></small></p>
     `;
     cardBody.className = "card-body cardProducto__body";
-
-    //Seteo de botón
-
 
     //Agregaciones
     imageContainer.appendChild(cardImage);
