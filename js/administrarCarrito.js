524c85e6acff116b45c4f9e91c1b12bfb9045180
jorgesalo86@gmail.com <fabi4872@gmail.com>
2022-08-16 22:54:34 -0300

    Incorporacion de local storage

--- js/administrarCarrito.js
+++ js/administrarCarrito.js
@@ -1,41 +1,23 @@
-class ProductoCarrito{
-    constructor(producto){
-        this.Imagen = producto.Imagen, 
-        this.Marca = producto.Marca, 
-        this.Modelo = producto.Modelo, 
-        this.Descripcion = producto.Descripcion, 
-        this.Precio = producto.Precio,
-        this.Envio = producto.Envio,
-        this.SubTotalPrecio = producto.Precio,
-        this.SubTotalEnvio = producto.Envio, 
-        this.Cantidad = 1
-    }
-
-    actualizarSubTotalPrecio(precio){
-        this.SubTotalPrecio += precio;
-    }
-
-    actualizarSubTotalEnvio(envio){
-        this.SubTotalEnvio += envio;
-    }
-
-    actualizarCantidad(){
-        this.Cantidad = this.Cantidad + 1;
-    }
-}
-
-
-
-//Declaración de variables
-let productosCarrito = [];
-let botones = document.getElementsByClassName("cardProducto__boton");
-
-
+//Funciones
 function validarStockProductoActualizarBotonAgregar(indiceProducto, cantidadStock){
     if(cantidadStock < 1){
         botones[indiceProducto].remove();
         let botonesContainers = document.getElementsByClassName("cardProducto__botonContainer");
-        botonesContainers[indiceProducto].innerHTML += `<button class="btn btn-primary cardProducto__boton" disabled>Agregar</button>`;
+        botonesContainers[indiceProducto].innerHTML += `<button class="btn btn-primary cardProducto__boton disabled">Agregar</button>`;
+    }
+}
+
+
+function actualizarLocalStorageCarrito(){
+    //Actualizo localStorage
+    localStorage.setItem("carrito", JSON.stringify(productosCarrito));
+}
+
+
+function habilitarBotonLimpiador(){
+    let botonLimpiar = document.getElementById("btnLimpiar");
+    if(botonLimpiar.classList.contains("disabled")){
+        botonLimpiar.classList.remove("disabled");
     }
 }
 
@@ -43,18 +25,37 @@ function validarStockProductoActualizarBotonAgregar(indiceProducto, cantidadStoc
 function armarRetornarElementoCarrito(producto){
     let cardProductoCarrito = document.createElement("div");
     let cardProductoCarritoBody = document.createElement("div");
-    cardProductoCarrito.className = "card cardProductoCarrito";
+    cardProductoCarrito.className = "cardProductoCarrito";
     cardProductoCarritoBody.className = "card-body cardProductoCarrito__body";
     
     cardProductoCarritoBody.innerHTML = `
-        <h5 class="card-title cardProductoCarrito__marca">${producto.Marca}</h5>
-        <h6 class="card-subtitle mb-2 text-muted cardProductoCarrito__modelo">Modelo ${producto.Modelo}</h6>
-        <p class="card-text cardProductoCarrito__descripcion">${producto.Descripcion}</p>
-        <h6 class="cardProductoCarrito__precio">Precio unitario: <strong class="cardProductoCarrito__strongPrecio">ARS ${producto.Precio}</strong><h6/>
-        <h6 class="cardProductoCarrito__envio">Precio de envío: <strong class="cardProductoCarrito__strongEnvio">ARS ${producto.Envio}</strong><h6/>
-        <h6 class="cardProductoCarrito__cantidad">CANTIDAD: <strong class="cardProductoCarrito__strongCantidad">${producto.Cantidad}</strong><h6/>
-        <h6 class="cardProductoCarrito__subTotalEnvio">SUBTOTAL ENVIO: <strong class="cardProductoCarrito__strongSubTotalEnvio">ARS ${producto.SubTotalEnvio}</strong><h6/>
-        <h6 class="cardProductoCarrito__subTotalPrecio">SUBTOTAL PRECIO: <strong class="cardProductoCarrito__strongSubTotalPrecio">ARS ${producto.SubTotalPrecio}</strong><h6/>
+        <div class="cardProductoCarrito__imagenContainer">
+            <img class="cardProductoCarrito__imagen" src="${producto.Imagen}" alt="Imagen de producto" />
+        </div>
+        <div class="cardProductoCarrito__detalle flexVertical">
+            <h5 class="card-title cardProductoCarrito__marca">${producto.Marca.toUpperCase()}</h5>
+            <h6 class="card-subtitle mb-2 text-muted cardProductoCarrito__modelo">Modelo ${producto.Modelo.toUpperCase()}</h6>
+            <p class="card-text cardProductoCarrito__descripcion mt-2">${producto.Descripcion}</p>
+        </div>
+        <div class="cardProductoCarrito__precios flexVertical">
+            <h6 class="cardProductoCarrito__titulo">Envío</h6>
+            <h5 class="cardProductoCarrito__envio cardProductoCarrito__importe">ARS ${producto.Envio}</h5>
+            <h6 class="cardProductoCarrito__titulo mt-3">Precio</h6>
+            <h5 class="cardProductoCarrito__precio cardProductoCarrito__importe">ARS ${producto.Precio}</h5>
+        </div>
+        <div class="cardProductoCarrito__subTotales flexVertical">
+            <h6 class="cardProductoCarrito__titulo">Subtotal Envío</h6>
+            <h5 class="cardProductoCarrito__subTotalEnvio cardProductoCarrito__importe">ARS ${producto.SubTotalEnvio}</h5>
+            <h6 class="cardProductoCarrito__titulo mt-3">Subtotal Precio</h6>
+            <h5 class="cardProductoCarrito__subTotalPrecio cardProductoCarrito__importe">ARS ${producto.SubTotalPrecio}</h5>
+        </div>
+        <div class="cardProductoCarrito__cantidades flexVertical">
+            <h6 class="cardProductoCarrito__titulo">Cantidad</h6>
+            <h5 class="cardProductoCarrito__cantidad">${producto.Cantidad}</h5>
+        </div>
+        <button class="cardProductoCarrito__botonDelete">
+            <i class="fa-solid fa-trash-can cardProductoCarrito__iconDelete"></i>
+        </button>
     `;
 
     cardProductoCarrito.appendChild(cardProductoCarritoBody);
@@ -64,14 +65,15 @@ function armarRetornarElementoCarrito(producto){
     
 
 function agregarProductoCarrito(producto, indiceProducto){
-    let productoExistenteCarrito = productosCarrito.find((prod) => prod.Descripcion === producto.Descripcion);
+    let productoExistenteCarrito = productosCarrito.find(prod => prod.Descripcion === producto.Descripcion);
     let seccionCarrito = document.getElementById("seccionCarrito");
 
     if(productoExistenteCarrito == undefined){
-        let productoCarrito = new ProductoCarrito(producto);
+        let productoCarrito = new Producto(producto.Imagen, producto.Marca, producto.Modelo, producto.Descripcion, producto.Cuotas, producto.Envio, producto.Precio, 1);
         productosCarrito.push(productoCarrito);
         let nuevoElementoCarrito = armarRetornarElementoCarrito(productoCarrito);
         seccionCarrito.appendChild(nuevoElementoCarrito);
+        habilitarBotonLimpiador();
     }
     else{
         productoExistenteCarrito.actualizarSubTotalPrecio(producto.Precio);
@@ -86,25 +88,59 @@ function agregarProductoCarrito(producto, indiceProducto){
             }
         }
 
-        let strongCantidades = document.getElementsByClassName("cardProductoCarrito__strongCantidad");
-        let strongSubTotalesEnvios = document.getElementsByClassName("cardProductoCarrito__strongSubTotalEnvio");
-        let strongSubTotalesPrecios = document.getElementsByClassName("cardProductoCarrito__strongSubTotalPrecio");
+        let cantidades = document.getElementsByClassName("cardProductoCarrito__cantidad");
+        let envios = document.getElementsByClassName("cardProductoCarrito__subTotalEnvio");
+        let precios = document.getElementsByClassName("cardProductoCarrito__subTotalPrecio");
         
-        strongCantidades[indiceProductoCarrito].innerHTML = productoExistenteCarrito.Cantidad;
-        strongSubTotalesEnvios[indiceProductoCarrito].innerHTML = `ARS ${productoExistenteCarrito.SubTotalEnvio}`;
-        strongSubTotalesPrecios[indiceProductoCarrito].innerHTML = `ARS ${productoExistenteCarrito.SubTotalPrecio}`;
+        cantidades[indiceProductoCarrito].innerHTML = productoExistenteCarrito.Cantidad;
+        envios[indiceProductoCarrito].innerHTML = `ARS ${productoExistenteCarrito.SubTotalEnvio}`;
+        precios[indiceProductoCarrito].innerHTML = `ARS ${productoExistenteCarrito.SubTotalPrecio}`;
     }
 
-    producto.Stock = producto.Stock - 1;
+    producto.Cantidad -= 1;
     let cantidadesStock = document.getElementsByClassName("cardProducto__stock");
-    cantidadesStock[indiceProducto].innerHTML = producto.Stock;
+    cantidadesStock[indiceProducto].innerHTML = producto.Cantidad;
 
-    validarStockProductoActualizarBotonAgregar(indiceProducto, producto.Stock);
+    validarStockProductoActualizarBotonAgregar(indiceProducto, producto.Cantidad);
+    actualizarLocalStorageCarrito();
+}
+
+
+
+
+
+
+
+//Declaración de variables
+let productosCarrito = [];
+let botones = document.getElementsByClassName("cardProducto__boton");
+
+
+//Recuperación de productos del carrito del localStorage
+if(localStorage.getItem("carrito")){
+    habilitarBotonLimpiador();
+    resultado = JSON.parse(localStorage.getItem("carrito"));
+    resultado.forEach(producto => {
+        let productoCarrito = new Producto(producto.Imagen, producto.Marca, producto.Modelo, producto.Descripcion, producto.Cuotas, producto.Envio, producto.Precio, producto.Cantidad);
+        productoCarrito.actualizarSubTotalPrecio(producto.Precio * producto.Cantidad);
+        productoCarrito.actualizarSubTotalEnvio(producto.Envio * producto.Cantidad);
+        productosCarrito.push(productoCarrito);
+    });
+    let seccionCarrito = document.getElementById("seccionCarrito");
+    for(const producto of productosCarrito){
+        let productoExistente = productos.find(prod => prod.Descripcion === producto.Descripcion);
+        let nuevoElementoCarrito = armarRetornarElementoCarrito(producto);
+        seccionCarrito.appendChild(nuevoElementoCarrito);
+        productoExistente.Cantidad -= producto.Cantidad;
+        let cantidadesStock = document.getElementsByClassName("cardProducto__stock");
+        let indiceProducto = productos.indexOf(productoExistente);
+        cantidadesStock[indiceProducto].innerHTML = productoExistente.Cantidad;
+    }
 }
 
 
 for(let i=0; i<botones.length; i++){
-    validarStockProductoActualizarBotonAgregar(i, productos[i].Stock);
+    validarStockProductoActualizarBotonAgregar(i, productos[i].Cantidad);
 
     botones[i].onclick = () => {    
         agregarProductoCarrito(productos[i], i);
--- js/cargarSeccionProductos.js
+++ js/cargarSeccionProductos.js
@@ -1,34 +1,10 @@
-class Producto{
-    constructor(imagen, marca, modelo, descripcion, cuotas, envio, precio, stock){
-        this.Imagen = imagen,
-        this.Marca = marca.toUpperCase(),
-        this.Modelo = modelo.toUpperCase(),
-        this.Descripcion = descripcion,
-        this.Cuotas = cuotas,
-        this.Envio = envio,
-        this.Precio = precio,
-        this.Stock = stock
-    }
-}
-
-
-
-const productos = [];
-
-const producto1 = new Producto("assets/kurzweil_sp2x.jpg", "Kurzweil", "Sp2x", "Piano eléctrico Kurzweil Sp2x, 88 teclas con peso piano martillo", 6, 1500, 200000, 10);
-const producto2 = new Producto("assets/cort_x250_bk.jpg", "Cort", "X250-bk", "Guitarra eléctrica Cort X250-BK, cuerpo sólido con dos cutaways", 12, 700, 300000, 5);
-const producto3 = new Producto("assets/knight_jbts_100.jpg", "Knight", "Jbts-100", "Saxo tenor Knight JBTS-100 Laqueado, llave de F#", 9, 2500, 400000, 2);
-const producto4 = new Producto("assets/focusrite_scarlett_2i4.jpg", "Focusrite", "Scarlett 2i4", "Placa de sonido Focusrite Scarlett 2i4 USB", 12, 350, 100000, 25);
-
-productos.push(producto1, producto2, producto3, producto4);
-
 let seccionProductos = document.getElementById("seccionProductos");
 
-productos.forEach((producto) => {
+productos.forEach(producto => {
     let card = document.createElement("div");
     let imageContainer = document.createElement("div");
-    let cardImage = document.createElement("img");
-    let cardBody = document.createElement("div");
+    let image = document.createElement("img");
+    let body = document.createElement("div");
 
     //Seteo de clase de la tarjeta
     card.className = "col-12 col-md-6 col-lg-3 card cardProducto";
@@ -37,27 +13,27 @@ productos.forEach((producto) => {
     imageContainer.className = "cardProducto__imageContainer";
 
     //Seteo de atributos y clase de la imagen
-    cardImage.setAttribute("src", producto.Imagen);
-    cardImage.setAttribute("alt", "Imagen de producto");
-    cardImage.className = "card-img-top cardProducto__imagen";
+    image.setAttribute("src", producto.Imagen);
+    image.setAttribute("alt", "Imagen de producto");
+    image.className = "card-img-top cardProducto__imagen";
 
     //Seteo de innerHTML para el cuerpo
     let valorCuota = Math.round(producto.Precio/producto.Cuotas);
-    cardBody.innerHTML = `
-        <h5 class="card-title cardProducto__marca">${producto.Marca}</h5>
-        <h6 class="card-subtitle mb-2 text-muted cardProducto__modelo">Modelo ${producto.Modelo}</h6>
+    body.innerHTML = `
+        <h5 class="card-title cardProducto__marca">${producto.Marca.toUpperCase()}</h5>
+        <h6 class="card-subtitle mb-2 text-muted cardProducto__modelo">Modelo ${producto.Modelo.toUpperCase()}</h6>
         <p class="card-text cardProducto__descripcion">${producto.Descripcion}</p>
         <h3 class="cardProducto__precio"><strong class="cardProducto__strongPrecio">ARS ${producto.Precio}</strong><h3/>
         <p class="card-text cardProducto__info"><i class="fa-solid fa-credit-card cardProducto__icon"></i><small class="text-muted">${producto.Cuotas} cuotas sin interés de <strong class="cardProducto__strong">ARS ${valorCuota}</strong></small></p>
         <p class="card-text cardProducto__info"><i class="fa-solid fa-truck cardProducto__icon"></i><small class="text-muted">Costo de envío: <strong class="cardProducto__strong">ARS ${producto.Envio}</strong></small></p>
-        <p class="card-text cardProducto__info"><i class="fa-solid fa-business-time cardProducto__icon"></i><small class="text-muted">Cantidad en stock: <strong class="cardProducto__stock cardProducto__strong">${producto.Stock}</strong></small></p>
+        <p class="card-text cardProducto__info"><i class="fa-solid fa-business-time cardProducto__icon"></i><small class="text-muted">Cantidad en stock: <strong class="cardProducto__stock cardProducto__strong">${producto.Cantidad}</strong></small></p>
     `;
-    cardBody.className = "card-body cardProducto__body";
+    body.className = "card-body cardProducto__body";
 
     //Agregaciones
-    imageContainer.appendChild(cardImage);
+    imageContainer.appendChild(image);
     card.appendChild(imageContainer);
-    card.appendChild(cardBody);
+    card.appendChild(body);
     card.innerHTML += `<div class="cardProducto__botonContainer"><button class="btn btn-primary cardProducto__boton">Agregar</button></div>`;
     seccionProductos.appendChild(card);
 });
